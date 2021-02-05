3948ed27703cc21b77986839c60a2fc52781b10e
Weikun Han <weikunhan@g.ucla.edu>
2021-02-04 22:00:00 -0800

    update 282

--- practice/solution/0282_expression_add_operators.py
+++ practice/solution/0282_expression_add_operators.py
@@ -8,21 +8,21 @@ class Solution(object):
         
         self.res = []
         
-        self.dfs(num, target, 0, None, '')
+        self.dfs(num, target, 0, None, [])
         
         return self.res
         
-    def dfs(self, num, target, sum_value, prenum_value, temp_value):            
+    def dfs(self, num, target, sum_value, temp_value, value_list):            
         if not num and sum_value == target:
-            self.res.append(temp_value)
+            self.res.append(''.join(value_list))
 
         for i in range(1, len(num) + 1):
             num_value = int(num[:i])
             
             if i == 1 or (i > 1 and num[0] != '0'):
-                if prenum_value == None:
-                    self.dfs(num[i:], target, num_value, num_value, num[:i])
+                if temp_value == None:
+                    self.dfs(num[i:], target, num_value, num_value, value_list + [num[:i]])
                 else:
-                    self.dfs(num[i:], target, sum_value + num_value, num_value, temp_value + '+' + num[:i])
-                    self.dfs(num[i:], target, sum_value - num_value, -num_value, temp_value + '-' + num[:i])
-                    self.dfs(num[i:], target, sum_value - prenum_value + prenum_value * num_value, prenum_value * num_value, temp_value + '*' + num[:i])
+                    self.dfs(num[i:], target, sum_value + num_value, num_value, value_list + ['+'] + [num[:i]])
+                    self.dfs(num[i:], target, sum_value - num_value, -num_value, value_list + ['-'] + [num[:i]])
+                    self.dfs(num[i:], target, sum_value - temp_value + temp_value * num_value, temp_value * num_value, value_list + ['*'] + [num[:i]])
