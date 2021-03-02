0dead618bbe992bba12626e4eb60b0a3bb1cdb61
Weikun Han <weikunhan@g.ucla.edu>
2021-03-02 00:55:32 -0800

    udpate 1102

--- practice/solution/1102_path_with_maximum_minimum_value.py
+++ practice/solution/1102_path_with_maximum_minimum_value.py
@@ -8,19 +8,19 @@ class Solution(object):
         """
         
         value_pq = []
-        heapq.heappush(value_pq, (-A[-1][-1], len(A) - 1, len(A[0]) - 1))
-        A[-1][-1] = -1
-        res = A[0][0]
+        heapq.heappush(value_pq, (-A[0][0], 0, 0))
+        A[0][0] = -1
+        res = A[-1][-1]
         
         while value_pq:
             cost, i, j = heapq.heappop(value_pq)
             res = min(res, -cost)
             
-            for a, b in [(i + 1, j), (i, j + 1), (i - 1, j), (i, j - 1)]:
-                if not i and not j:
+            if i == len(A) - 1 and j == len(A[0]) - 1:
 
-                    return res
-                
+                return res
+            
+            for a, b in [(i + 1, j), (i, j + 1), (i - 1, j), (i, j - 1)]:
                 if a >= 0 and a < len(A) and b >= 0 and b < len(A[0]) and A[a][b] != -1:
                     heapq.heappush(value_pq, (-A[a][b], a, b))
                     A[a][b] = -1
