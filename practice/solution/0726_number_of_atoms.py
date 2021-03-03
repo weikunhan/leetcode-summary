b036d40bed6ffe615c422f35710171893711526e
Weikun Han <weikunhan@g.ucla.edu>
2021-03-03 15:07:53 -0800

    update

--- practice/solution/0726_number_of_atoms.py
+++ practice/solution/0726_number_of_atoms.py
@@ -16,16 +16,23 @@ class Solution(object):
         res = ''
         
         for char in formula[::-1]:
-            if char == ")":
+            if char == ')':
                 if not sum_value:
-                    value_stack.append(1)
-                    temp_value *= 1
+                    value_stack.append(temp_value)
                 else:
                     value_stack.append(sum_value)
                     temp_value *= sum_value
     
                 sum_value = 0
                 count = 0
+            elif char == '(':
+                sum_value = value_stack.pop()
+                temp_value //= sum_value
+                sum_value = 0
+                count = 0
+            elif char.isdigit():
+                sum_value += int(char) * (10 ** count)
+                count += 1
             elif char.isupper():
                 word_value += char
                 
@@ -34,18 +41,10 @@ class Solution(object):
                 else:
                     value_dict[word_value[::-1]] += sum_value * temp_value
                     
-                word_value = ""
+                word_value = ''
                 sum_value = 0
                 count = 0
-            elif char == "(":
-                sum_value = value_stack.pop()
-                temp_value //= sum_value
-                sum_value = 0
-                count = 0
-            elif char.isdigit():
-                sum_value += int(char) * (10 ** count)
-                count += 1
-            elif char.islower():
+            else:
                 word_value += char
                 
         for key, value in sorted(value_dict.items()):
