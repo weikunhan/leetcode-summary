7b7fbcd507246607157d915bb005f99ed5416a2a
Weikun Han <weikunhan@g.ucla.edu>
2021-03-17 00:07:59 -0700

    update

--- practice/solution/0044_wildcard_matching.py
+++ practice/solution/0044_wildcard_matching.py
@@ -6,8 +6,8 @@ class Solution(object):
         :rtype: bool
         """
         
-        row_end = len(s)
-        col_end = len(p)
+        row_end = len(p)
+        col_end = len(s)
         dp_list = [[False] * (col_end + 1) for _ in range(row_end + 1)]
         dp_list[0][0] = True
         res = False
@@ -16,12 +16,12 @@ class Solution(object):
             if p[i] != '*':
                 break
                 
-            dp_list[0][i + 1] = True
+            dp_list[i + 1][0] = True
                 
-        for i in range(len(s)):
-            for j in range(len(p)):
-                if p[j] != '*':
-                    if dp_list[i][j] and (p[j] ==s[i] or p[j] == '?'):
+        for i in range(len(p)):
+            for j in range(len(s)):
+                if p[i] != '*':
+                    if dp_list[i][j] and (p[i] ==s[j] or p[i] == '?'):
                         dp_list[i + 1][j + 1] = True
                 else:
                     if dp_list[i][j + 1]:
