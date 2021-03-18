0904f3b85a07f032dc0a94231d5d22c0dc8b5fb2
Weikun Han <weikunhan@g.ucla.edu>
2021-03-18 13:53:33 -0700

    update

--- practice/solution/1262_greatest_sum_divisible_by_three.py
+++ practice/solution/1262_greatest_sum_divisible_by_three.py
@@ -9,9 +9,7 @@ class Solution(object):
         res = 0
         
         for num in nums:
-            value_list = [dp_list[0], dp_list[1], dp_list[2]]
-            
-            for value in value_list:
+            for value in [dp_list[0], dp_list[1], dp_list[2]]:
                 dp_list[(num + value) % 3] = max(dp_list[(num + value) % 3], num + value)
                 
         res = dp_list[0]
