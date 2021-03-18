f707dc902bb51aa50edd248fe7f7acd79877cccd
Weikun Han <weikunhan@g.ucla.edu>
2021-03-17 23:53:29 -0700

    update

--- practice/solution/0282_expression_add_operators.py
+++ practice/solution/0282_expression_add_operators.py
@@ -12,7 +12,7 @@ class Solution(object):
         
         return self.res
         
-    def dfs(self, num, target, sum_value, temp_value, value_list):            
+    def dfs(self, num, target, sum_value, prenum_value, value_list):            
         if not num and sum_value == target:
             self.res.append(''.join(value_list))
 
@@ -20,9 +20,9 @@ class Solution(object):
             num_value = int(num[:i])
             
             if i == 1 or (i > 1 and num[0] != '0'):
-                if temp_value == None:
+                if prenum_value == None:
                     self.dfs(num[i:], target, num_value, num_value, value_list + [num[:i]])
                 else:
                     self.dfs(num[i:], target, sum_value + num_value, num_value, value_list + ['+'] + [num[:i]])
                     self.dfs(num[i:], target, sum_value - num_value, -num_value, value_list + ['-'] + [num[:i]])
-                    self.dfs(num[i:], target, sum_value - temp_value + temp_value * num_value, temp_value * num_value, value_list + ['*'] + [num[:i]])
+                    self.dfs(num[i:], target, sum_value - prenum_value + prenum_value * num_value, prenum_value * num_value, value_list + ['*'] + [num[:i]])
