8574ccf2d061188a1b9b42856f720bd56d55a355
Weikun Han <weikunhan@g.ucla.edu>
2021-03-21 09:28:12 -0700

    update

--- practice/solution/0079_word_search.py
+++ practice/solution/0079_word_search.py
@@ -10,22 +10,22 @@ class Solution(object):
         
         for i in range(len(board)):
             for j in range(len(board[0])):
-                self.dfs(i, j, board, 0, word)
+                self.dfs(i, j, board, word)
 
         return self.res
-    
-    def dfs(self, row, col, board, start, word):
-        if start == len(word):
+ 
+    def dfs(self, row, col, board, word):
+        if not word: 
             self.res = True
-        
-        if row < 0 or col < 0 or row >= len(board) or col >= len(board[0]) or self.res or board[row][col] != word[start]:
             
-            return
+        if row < 0 or col < 0 or row >= len(board) or col >= len(board[0]) or self.res or board[row][col] != word[0]:
+            
+            return 
         
         temp_value = board[row][col]
         board[row][col] = '#'
-        self.dfs(row + 1, col, board, start + 1, word)
-        self.dfs(row, col + 1, board, start + 1, word)
-        self.dfs(row - 1, col, board, start + 1, word)
-        self.dfs(row, col - 1, board, start + 1, word)
+        self.dfs(row + 1, col, board, word[1:])
+        self.dfs(row, col + 1, board, word[1:])
+        self.dfs(row - 1, col, board, word[1:])
+        self.dfs(row, col - 1, board, word[1:])
         board[row][col] = temp_value
