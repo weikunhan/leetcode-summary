dc45210cb2cc50bfefd8c21c865e6ee2163a022a
Weikun Han <weikunhan@g.ucla.edu>
2021-06-06 13:02:13 -0700

    update 1481

--- practice/solution/1481_least_number_of_unique_integers_after_k_removals.py
+++ practice/solution/1481_least_number_of_unique_integers_after_k_removals.py
@@ -14,14 +14,14 @@ class Solution(object):
         res = 0
         
         for key, value in value_dict.items():
-            heapq.heappush(value_pq, value)
-            
+            heapq.heappush(value_pq, (value, key))
+        
         while k > 0:
-            k -= heapq.heappop(value_pq)
-            
+            k -= heapq.heappop(value_pq)[0]
+        
         if k < 0:
             res = len(value_pq) + 1
         else:
             res = len(value_pq)
-            
+
         return res
